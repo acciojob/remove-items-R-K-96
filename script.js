2552e6e88f53b6b81489a513df9207154d5fffd5
R-K-96 <124651427+R-K-96@users.noreply.github.com>
2023-05-18 18:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+const removeBtn = document.querySelector("input");
+
+removeBtn.addEventListener("click",()=>{
+	const selectOption = document.getElementById("colorSelect");
+	selectOption.remove(selectOption.selectedIndex)
+})
+
